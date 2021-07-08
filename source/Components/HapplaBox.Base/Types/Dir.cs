65574103e88946afec109d874526f86a322fdc85
Phap Dieu Duong <d2phap@gmail.com>
2021-07-08 15:07:19 +0800

    add base project

---
+++ source/Components/HapplaBox.Base/Types/Dir.cs
@@ -0,0 +1,34 @@
+﻿
+namespace HapplaBox.Base
+{
+    /// <summary>
+    /// Directory name constants
+    /// </summary>
+    public static class Dir
+    {
+        /// <summary>
+        /// Gets folder name of theme packs
+        /// </summary>
+        public static string Themes { get; } = "themes";
+
+        /// <summary>
+        /// Gets folder name of assets
+        /// </summary>
+        public static string Assets { get; } = "assets";
+
+        /// <summary>
+        /// Gets folder name of language
+        /// </summary>
+        public static string Lang { get; } = "lang";
+
+        /// <summary>
+        /// Gets folder name of language
+        /// </summary>
+        public static string Codecs { get; } = "codecs";
+
+        /// <summary>
+        /// Gets folder name of temporary
+        /// </summary>
+        public static string Temp { get; } = "temp";
+    }
+}
