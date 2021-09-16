99c26fb0d429151d204095c3a1f8b8a96b0e8ed3
Phap Dieu Duong <d2phap@gmail.com>
2021-09-16 14:42:22 +0800

    toolbar: add name to item

--- source/Components/HapplaBox.Viewer/src/components/toolbar/types.ts
+++ source/Components/HapplaBox.Viewer/src/components/toolbar/types.ts
@@ -1,7 +1,7 @@
 
 export type HbToolbarItemType = 'button' | 'divider' | 'space';
 export type HbToolbarGroup = 'center' | 'bottom';
-export type HbToolbarClickFunc = (e: PointerEvent, itemIndex: number) => any;
+export type HbToolbarClickFunc = (e: PointerEvent, itemName: string) => any;
 export type HbToolbarRightClickFunc = (e: PointerEvent) => any;
 
 export interface HbToolbarItem {
@@ -11,18 +11,17 @@ export interface HbToolbarItem {
    * Returns true if the item is moved to Overflow dropdown
    */
   overflow?: boolean;
-}
-export interface HbToolbarDivider extends HbToolbarItem {};
-export interface HbToolbarButton extends HbToolbarItem {
-  label: string;
-  imageUrl: string;
+  cssClass?: string;
+
+  name?: string;
+  label?: string;
+  imageUrl?: string;
   tooltip?: string;
   checkable?: boolean;
   isChecked?: boolean;
   disabled?: boolean;
   clickFn?: HbToolbarClickFunc;
-};
-
+}
 
 export type HbToolbarPosition = 'top' | 'bottom';
 export interface HbToolbarOptions {
