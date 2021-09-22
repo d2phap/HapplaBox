1f7d7581c01b1d8a65b32a9b9632fa257708c504
Phap Dieu Duong <d2phap@gmail.com>
2021-09-22 22:05:55 +0800

    viewer: send event system theme changed to c# code

--- source/Components/HapplaBox.Viewer/src/pages/base.ts
+++ source/Components/HapplaBox.Viewer/src/pages/base.ts
@@ -1,2 +1,13 @@
 
 console.log('base');
+
+// on system theme changed
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  const newColorScheme = e.matches ? 'dark' : 'light';
+
+  console.log(newColorScheme);
+
+  // @ts-ignore
+  window.chrome.webview.postMessage(newColorScheme);
+});
+
