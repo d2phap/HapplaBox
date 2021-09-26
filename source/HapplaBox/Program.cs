c3933ddc211ad951586acf9ffa2f3198397dc6d1
Phap Dieu Duong <d2phap@gmail.com>
2021-09-26 12:07:45 +0800

    handle global ThreadException

--- source/HapplaBox/Program.cs
+++ source/HapplaBox/Program.cs
@@ -1,10 +1,35 @@
 
+using System.Threading;
 using System.Windows.Forms;
 using HapplaBox;
 using HapplaBox.Settings;
 
 ApplicationConfiguration.Initialize();
 Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+
+Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+Application.ThreadException += Application_ThreadException;
+
+void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+{
+    TaskDialog.ShowDialog(new()
+    {
+        Icon = TaskDialogIcon.Error,
+        Caption = $"Oops! {Application.ProductName} is encountering an error.",
+
+        Heading = e.Exception.Message,
+        Text = "Unhandled exception has occurred." +
+                "\r\nYou can click Continue to ignore this error, Copy to copy the error details, or Quit to exit the application.",
+        Expander = new(e.Exception.ToString()),
+        Buttons = new TaskDialogButtonCollection
+        {
+            new TaskDialogButton("Continue", allowCloseDialog: true),
+            new TaskDialogButton("Copy", allowCloseDialog: false),
+            new TaskDialogButton("Quit", allowCloseDialog: true),
+        },
+    });
+}
+
 
 // load application configs
 Config.Load();
