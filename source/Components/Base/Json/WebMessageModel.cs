d1210f289944cc9a96bce19e97248afbf8ad8428
Phap Dieu Duong <d2phap@gmail.com>
2021-09-28 11:30:21 +0800

    c#: json util to parse web msg

---
+++ source/Components/Base/Json/WebMessageModel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace HapplaBox.Base
+{
+    /// <summary>
+    /// Modal object for parsing message from webview client
+    /// </summary>
+    /// <typeparam name="T">Web message data type</typeparam>
+    public class WebMessageModel<T>
+    {
+        /// <summary>
+        /// Event name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Message data
+        /// </summary>
+        public T? Data { get; set; }
+    }
+}
