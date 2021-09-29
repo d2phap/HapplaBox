3268066a85ec671d70c8ff894367babea35f289c
Phap Dieu Duong <d2phap@gmail.com>
2021-09-29 13:56:30 +0800

    add codec manager

---
+++ source/Components/Codec/Codec/CodecManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace HapplaBox.Core.Codec
+{
+    public class CodecManager
+    {
+        public List<ICodec> Items { get; set; } = new List<ICodec>();
+
+
+        /// <summary>
+        /// Gets a codec
+        /// </summary>
+        /// <param name="codecId">Codec type fullname</param>
+        /// <returns></returns>
+        public ICodec? Get(string codecId)
+        {
+            var codec = this.Items.Find(item => item.GetType().FullName == codecId);
+
+            return codec;
+        }
+
+
+        /// <summary>
+        /// Loads all *.dll files and all codecs inside.
+        /// </summary>
+        /// <param name="codecDir"></param>
+        public void LoadAllCodecs(string codecDir)
+        {
+            Directory.CreateDirectory(codecDir);
+            var files = Directory.GetFiles(codecDir, "*.dll", SearchOption.TopDirectoryOnly);
+
+
+            this.Items = files.SelectMany(path =>
+            {
+                var pluginAssembly = LoadPlugin(path);
+
+                return CreateCodecs(pluginAssembly);
+            }).ToList();
+        }
+
+
+        /// <summary>
+        /// Loads all codecs in the assembly file
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        private IEnumerable<ICodec> CreateCodecs(Assembly assembly)
+        {
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (typeof(ICodec).IsAssignableFrom(type))
+                {
+                    if (Activator.CreateInstance(type) is ICodec result)
+                    {
+                        yield return result;
+                    }
+                }
+            }
+        }
+
+        private Assembly LoadPlugin(string relativePath)
+        {
+            // Navigate up to the solution root
+            var root = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            Path.GetDirectoryName(
+                                Path.GetDirectoryName(typeof(CodecManager).Assembly.Location)))))));
+
+            var pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
+
+            var loadContext = new DllLoadContext(pluginLocation);
+
+            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+        }
+    }
+}
---
+++ source/Components/Codec/Codec/ICodec.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+
+namespace HapplaBox.Core.Codec
+{
+
+    /// <summary>
+    /// Settings for loading image
+    /// </summary>
+    public struct CodecReadSettings
+    {
+        public int Width;
+        public int Height;
+        public bool IgnoreColorProfile;
+    }
+
+
+    /// <summary>
+    /// Provides interface of image codec
+    /// </summary>
+    public interface ICodec
+    {
+        /// <summary>
+        /// Full name of assembly type of this codec
+        /// </summary>
+        public string CodecId => this.GetType().FullName ?? string.Empty;
+
+        /// <summary>
+        /// Name of the codec
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Short description of the codec
+        /// </summary>
+        public string Description { get; }
+
+        /// <summary>
+        /// Author of the codec
+        /// </summary>
+        public string Author { get; }
+
+        /// <summary>
+        /// Get the supported extensions
+        /// </summary>
+        public List<string> SupportedExts { get; }
+
+        /// <summary>
+        /// Contact info of the codec author
+        /// </summary>
+        public string Contact { get; }
+
+
+        /// <summary>
+        /// Version of the codec
+        /// </summary>
+        public Version Version { get; }
+
+
+        /// <summary>
+        /// Version of the API used in writting the codec
+        /// </summary>
+        public Version ApiVersion { get; }
+
+
+        /// <summary>
+        /// Loads image file and returns BitmapSource
+        /// </summary>
+        /// <param name="filename">Full path of the file</param>
+        /// <param name="settings">Loading settings</param>
+        /// <returns></returns>
+        public Bitmap Load(string filename, CodecReadSettings settings);
+    }
+}
