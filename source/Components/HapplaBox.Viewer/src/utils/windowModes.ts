61ddc2c42566aef44301cfb4656cd236ab9c92df
Phap Dieu Duong <ddphap@gmail.com>
2021-10-20 18:19:50 +0800

    ui: add types for window / zoom modes

---
+++ source/Components/HapplaBox.Viewer/src/utils/windowModes.ts
@@ -0,0 +1,9 @@
+
+const windowModes = {
+  fullScreen: 'FullScreen',
+  slideshow: 'Slideshow',
+  windowFit: 'WindowFit',
+  frameless: 'Frameless',
+};
+
+export default windowModes;
---
+++ source/Components/HapplaBox.Viewer/src/utils/zoomModes.ts
@@ -0,0 +1,11 @@
+
+const zoomModes = {
+  autoZoom: 'AutoZoom',
+  lockZoom: 'LockZoom',
+  scaleToWidth: 'ScaleToWidth',
+  scaleToHeight: 'ScaleToHeight',
+  scaleToFit: 'ScaleToFit',
+  scaleToFill: 'ScaleToFill',
+};
+
+export default zoomModes;
