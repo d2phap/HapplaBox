cda1e2d30e6c1ab29d2fb973298b9b3889376c82
Phap Dieu Duong <ddphap@gmail.com>
2021-11-13 14:40:23 +0800

    be: add codec: HapplaBox.Codec.System

---
+++ source/Codecs/HapplaBox.Codec.System/Codec.cs
@@ -0,0 +1,47 @@
+﻿using HapplaBox.Core.Codec;
+using System.Drawing.Imaging;
+
+namespace HapplaBox.Codec.System
+{
+    public class Codec : ICodec
+    {
+        public string Name => "System codec";
+
+        public string Description => "Use default system codec to decode file formats";
+
+        public string Author => "Duong Dieu Phap";
+        public string Contact => "phap@imageglass.org";
+        public Version Version => new(1, 0, 0, 0);
+
+        public Version ApiVersion => new(0, 5);
+
+        public List<string> SupportedExts => new(2)
+        {
+            ".jpg",
+            ".png",
+        };
+
+        public byte[] GetThumbnail(string filename, int width, int height)
+        {
+            using var bmp = new Bitmap(filename);
+            using var thumb = bmp.GetThumbnailImage(width, height, null, IntPtr.Zero);
+
+            using var ms = new MemoryStream();
+            thumb.Save(ms, ImageFormat.Png);
+
+            return ms.ToArray();
+        }
+
+        public string GetThumbnailBase64(string filename, int width, int height)
+        {
+            var bytes = GetThumbnail(filename, width, height);
+
+            return "data:image/png;base64," + Convert.ToBase64String(bytes);
+        }
+
+        public Bitmap Load(string filename, CodecReadSettings settings)
+        {
+            return new Bitmap(filename);
+        }
+    }
+}
